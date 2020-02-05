509d6246755db7dbd1556c31b23e334b6625cf85
kppapenh <60671423+kppapenh@users.noreply.github.com>
2020-02-05 09:04:15 -0500

    Create Run10.py

---
+++ Run10.py
@@ -0,0 +1,25 @@
+# Runs a program multiple times
+#!/bin/bash
+
+python handshake.py 0 &
+
+python handshake.py 1000 &
+
+python handshake.py 2000 &
+
+python handshake.py 3000 &
+
+python handshake.py 4000 &
+
+python handshake.py 5000 &
+
+python handshake.py 6000 &
+
+python handshake.py 7000 &
+
+python handshake.py 8000 &
+
+python handshake.py 9000 &
+
+
+# runs multiple instances of a program
